d38dd9a772ad84669e7ed234de4962216259fbc5
Michael Mollard <micheal.mollard@swan.io>
2024-12-05 13:02:26 +0900

    chore: update images

---
+++ src/components/projects.ts
@@ -0,0 +1,31 @@
+import logoImg from "../assets/rei-green.png";
+import selectraImg from "../assets/selectra.png";
+import artImg from "../assets/art.png";
+
+export const projects = [
+	{
+		id: "selectra",
+		title: "料金比較のセレクトラ",
+		image: selectraImg,
+		description: "料金比較サイト「セレクトラ」のUI/UX改善",
+		link: "/pdf/selectra.pdf",
+		slideLink:
+			"https://docs.google.com/presentation/d/e/2PACX-1vT33otSCt5AzZBM_MrFUYPOizabb0WLMtgY8ksN2ELSJW93j0Xpnl3WPIm-Ge1lwjFuBLC999h5NMEu/pub?start=false&loop=false&delayms=3000",
+	},
+	{
+		id: "artful",
+		title: "Artfull",
+		image: artImg,
+		description: "【架空】アートギャラリー向けスマホアプリ",
+		link: "/pdf/artful.pdf",
+		slideLink:
+			"https://docs.google.com/presentation/d/e/2PACX-1vRMIEGfJQ1kB78RkgIkb4CMvSkwrH4vOvHc_ZGb0CITCZrb6j-fJbaQ5Gfbrbcw2Ttw6TRkqWcmo_b7/pub?start=false&loop=false&delayms=3000",
+	},
+	{
+		id: "logo",
+		title: "グラフィック制作",
+		image: logoImg,
+		description: "Blenderで3Dロゴタイプを作成",
+		link: "/logo",
+	},
+];
