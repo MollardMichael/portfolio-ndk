ced729361fa862520e5c2c9afe557aa602ac5436
Mollard Michael <michamfr@gmail.com>
2025-06-10 23:27:06 +0900

    feat: add more projects

--- src/components/projects.ts
+++ src/components/projects.ts
@@ -3,45 +3,61 @@ import selectraImg from "../assets/selectra.png";
 import artImg from "../assets/artful/art.png";
 
 type Project = {
-	id: string;
-	title: string;
-	image: ImageMetadata;
-	description: string;
-	link: string;
-	tags: { mode: "primary" | "secondary"; name: string }[];
-	slideLink?: string;
+  id: string;
+  title: string;
+  image: ImageMetadata;
+  description: string;
+  link: string;
+  tags: { mode: "primary" | "secondary"; name: string }[];
+  slideLink?: string;
 };
 
 export const projects = [
-	{
-		id: "selectra",
-		title: "料金比較のセレクトラ",
-		image: selectraImg,
-		description: "料金比較サイト「セレクトラ」のUI/UX改善",
-		link: "/pdf/selectra.pdf",
-		slideLink:
-			"https://docs.google.com/presentation/d/e/2PACX-1vT33otSCt5AzZBM_MrFUYPOizabb0WLMtgY8ksN2ELSJW93j0Xpnl3WPIm-Ge1lwjFuBLC999h5NMEu/pub?start=false&loop=false&delayms=3000",
-		tags: [{ name: "UX Design", mode: "secondary" }],
-	},
-	{
-		id: "artful",
-		title: "Artfull",
-		image: artImg,
-		description: "アートギャラリーのためのモバイルアプリ",
-		link: "/projects/artful",
-		slideLink:
-			"https://docs.google.com/presentation/d/e/2PACX-1vRMIEGfJQ1kB78RkgIkb4CMvSkwrH4vOvHc_ZGb0CITCZrb6j-fJbaQ5Gfbrbcw2Ttw6TRkqWcmo_b7/pub?start=false&loop=false&delayms=3000",
-		tags: [
-			{ name: "UI Design", mode: "primary" },
-			{ name: "UX Design", mode: "secondary" },
-		],
-	},
-	{
-		id: "logo",
-		title: "グラフィック制作",
-		image: logoImg,
-		description: "Blenderで3Dロゴタイプを作成",
-		link: "/logo",
-		tags: [{ name: "Graphics", mode: "primary" }],
-	},
+  {
+    id: "artful",
+    title: "Artfull",
+    image: artImg,
+    description: "アートギャラリーのためのモバイルアプリ",
+    link: "/projects/artful",
+    tags: [
+      { name: "UI Design", mode: "primary" },
+      { name: "UX Design", mode: "secondary" },
+    ],
+  },
+  {
+    id: "portfolio",
+    title: "ポートフォリオ",
+    image: artImg,
+    description:
+      "UXデザインポートフォリオのためのUXデザイン・デザインシステム作成およびアートディレクション",
+    link: "/projects/portfolio",
+    tags: [{ name: "UI Design", mode: "primary" }],
+  },
+  {
+    id: "localization",
+    title: "日本語サイト向けリデザイン実例",
+    image: artImg,
+    description:
+      "日本語サイト向けにページのデザインを変更・主に欧文・和文における行間の視覚的差異に着目しリサーチ・A/Bテストを実施",
+    link: "/projects/localization",
+    tags: [{ name: "UX Design", mode: "primary" }],
+  },
+  {
+    id: "selectra",
+    title: "料金比較のセレクトラ",
+    image: selectraImg,
+    description: "料金比較サイト「セレクトラ」のUI/UX改善",
+    link: "/pdf/selectra.pdf",
+    slideLink:
+      "https://docs.google.com/presentation/d/e/2PACX-1vT33otSCt5AzZBM_MrFUYPOizabb0WLMtgY8ksN2ELSJW93j0Xpnl3WPIm-Ge1lwjFuBLC999h5NMEu/pub?start=false&loop=false&delayms=3000",
+    tags: [{ name: "UX Design", mode: "secondary" }],
+  },
+  {
+    id: "logo",
+    title: "グラフィック制作",
+    image: logoImg,
+    description: "Blenderで3Dロゴタイプを作成",
+    link: "/projects/logo",
+    tags: [{ name: "Graphics", mode: "primary" }],
+  },
 ] satisfies Project[];
